2aed6e04eaf02335ec400cad04d077bc1eab817a
jLuseno161 <joyluseno2@gmail.com>
2021-06-18 22:16:28 +0300

    add authentication functions

---
+++ app/auth/views.py
@@ -0,0 +1,54 @@
+from flask import flash, redirect, render_template, request, url_for
+from flask_login import login_required, login_user, logout_user
+from .. import db
+from ..models import User
+from . import auth
+from .forms import LoginForm, RegistrationForm
+from ..email import mail_message
+
+#login form
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter_by(email=login_form.email.data).first()
+        if user is not None and user.verify_password(login_form.password.data):
+            login_user(user, login_form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Pitchfest"
+    return render_template('auth/login.html',
+                           login_form=login_form,
+                           title=title)
+
+
+#register function
+@auth.route('/register', methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        mail_message("Welcome to Pitchfest",
+                     "email/welcome_user",
+                     user.email,
+                     user=user)
+        return redirect(url_for('auth.login'))
+    title = "New Account"
+
+    print(mail_message)
+
+    return render_template('auth/register.html', registration_form=form)
+
+#logout form
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('You have been successfully logged out')
+    return redirect(url_for("main.index"))
