a1e11fd539fdb019f1b12913c500045a10e73836
jLuseno161 <joyluseno2@gmail.com>
2021-06-21 07:03:05 +0300

    quote test class done

---
+++ tests/test_quote.py
@@ -0,0 +1,13 @@
+import unittest
+from app.models import Quote
+
+class TestQuote(unittest.TestCase):
+    def setUp(self):
+        self.random_quote = Quote("Jojoy", "To blog is to vlog")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.random_quote, Quote))
+
+    def test_init(self):
+        self.assertEqual(self.random_quote.author, "Jojoy")
+        self.assertEqual(self.random_quote.quote,"To blog is to vlog")
