9f664fccb3be12448d16296e1afba4edb3b176ed
jLuseno161 <joyluseno2@gmail.com>
2021-06-21 07:24:28 +0300

    deploy to heroku

--- config.py
+++ config.py
@@ -1,20 +1,22 @@
 
 import os
 
+
 class Config:
 
     SECRET_KEY = os.environ.get('SECRET_KEY')
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
 
-    #email configutions
+    # email configutions
     # MAIL_SERVER = 'smtp.googlemail.com'
     # MAIL_PORT = 587
     # MAIL_USE_TLS =True
     # MAIL_USERNAME=os.environ.get("MAIL_USERNAME")
     # MAIL_PASSWORD=os.environ.get("MAIL_PASSWORD")
-    
+
     SIMPLEMDE_JS_IIFE = True
     SIMPLEMDE_USE_CDN = True
+
     @staticmethod
     def init_app(app):
         pass
@@ -23,7 +25,11 @@ class Config:
 
 
 class ProdConfig(Config):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
+        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
+            "postgres://", "postgresql://")
+
 
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:jluseno@localhost/pinblog'
@@ -34,4 +40,3 @@ config_options = {
     'development': DevConfig,
     'production': ProdConfig,
 }
-
